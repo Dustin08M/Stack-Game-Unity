1ba7a44fd811ef04d0cad1dd1c743d9cc4db28db
WindyB <dustin.macorol@ciit.edu.ph>
2026-03-12 20:08:00 +0800

    move block obj

---
+++ Assets/Scripts/Function_Extension.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Function_Extension
+{
+    public static bool Rndmz_BlockMovement()
+    {
+        return Random.value > 0.5f;
+    }
+}
---
+++ Assets/Scripts/MoveBlock.cs
@@ -0,0 +1,48 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBlock : MonoBehaviour
+{
+    public int blckSpeed = 3;
+    public bool IsHorizontal;
+
+    private void Start()
+    {
+        IsHorizontal = Function_Extension.Rndmz_BlockMovement(); //if false, move forward, if true move horizontal
+        Debug.Log($"Value is {IsHorizontal}");
+    }
+
+    void Update()
+    {
+        if (isMovingHor())
+        {
+            _gmFnc_MoveBlockLeft();
+        }
+        else
+            _gmFnc_MoveBlockForward();
+    }
+    bool isMovingHor()
+    {
+        return IsHorizontal;
+    }
+
+    //"_gmFnc" simply means "Game Function", followed by name of function
+    void _gmFnc_MoveBlockLeft()
+    {
+        transform.position += Vector3.left * blckSpeed * Time.deltaTime;
+        if (transform.position.x <= -2f)
+            blckSpeed = -3;
+        if(transform.position.x >= 2f)
+            blckSpeed = 3;
+    }
+    void _gmFnc_MoveBlockForward()
+    {
+        transform.position += Vector3.forward * blckSpeed * Time.deltaTime;
+        if (transform.position.z >= 2f)
+            blckSpeed = -3;
+        if (transform.position.z <= -2f)
+            blckSpeed = 3;
+    }
+}
